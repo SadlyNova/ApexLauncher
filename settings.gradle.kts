c27d2a22aed0038777dc71c4a10ce41bb69b2e6d
SadlyNova <novalauncher1.10@gmail.com>
2026-05-21 23:26:23 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Nova Launcher"
+rootProject.name = "Zalith Launcher"
 include(":jre_lwjgl3glfw")
-include(":NovaLauncher")
+include(":ZailthLauncher")
