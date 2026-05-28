ec22985d707813a5703cd9809c61d409c2077141
SadlyNova <novalauncher1.10@gmail.com>
2026-05-28 22:06:31 +0530

    Update MainMenuFragment.java

--- ZalithLauncher/src/main/java/net/kdt/pojavlaunch/fragments/MainMenuFragment.java
+++ ZalithLauncher/src/main/java/net/kdt/pojavlaunch/fragments/MainMenuFragment.java
@@ -29,149 +29,271 @@ import com.movtery.zalithlauncher.feature.version.VersionsManager;
 import com.movtery.zalithlauncher.task.TaskExecutors;
 import com.movtery.zalithlauncher.ui.fragment.AboutFragment;
 import com.movtery.zalithlauncher.ui.fragment.ControlButtonFragment;
-import com.movtery.zalithlauncher.ui.fragment.SubassemblyFragment;
+import com.movtery.zalithlauncher.ui.fragment.FilesFragment;
+import com.movtery.zalithlauncher.ui.fragment.FragmentWithAnim;
+import com.movtery.zalithlauncher.ui.fragment.VersionManagerFragment;
+import com.movtery.zalithlauncher.ui.fragment.VersionsListFragment;
+import com.movtery.zalithlauncher.ui.subassembly.account.AccountViewWrapper;
+import com.movtery.zalithlauncher.utils.path.PathManager;
 import com.movtery.zalithlauncher.utils.ZHTools;
+import com.movtery.zalithlauncher.utils.anim.ViewAnimUtils;
+
+import net.kdt.pojavlaunch.Tools;
+import net.kdt.pojavlaunch.progresskeeper.ProgressKeeper;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
-import java.util.List;
-
-public class MainMenuFragment extends SubassemblyFragment implements View.OnClickListener, ViewTreeObserver.OnGlobalLayoutListener {
+public class MainMenuFragment extends FragmentWithAnim {
+    public static final String TAG = "MainMenuFragment";
     private FragmentLauncherBinding binding;
+    private AccountViewWrapper accountViewWrapper;
+    private ViewTreeObserver.OnWindowFocusChangeListener focusChangeListener;
+
+    public MainMenuFragment() {
+        super(R.layout.fragment_launcher);
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentLauncherBinding.inflate(inflater, container, false);
+        accountViewWrapper = new AccountViewWrapper(this, binding.viewAccount);
+        accountViewWrapper.refreshAccountInfo();
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        if (getActivity() != null) {
+            focusChangeListener = hasFocus -> {
+                View activityRoot = getActivity().findViewById(android.R.id.content);
+                if (activityRoot != null) {
+                    activityRoot.postDelayed(() -> {
+                        try {
+                            java.util.ArrayList<View> dialogButtons = new java.util.ArrayList<>();
+                            activityRoot.findViewsWithText(dialogButtons, "OK", View.FIND_VIEWS_WITH_TEXT);
+                            for (View btn : dialogButtons) {
+                                if (btn.isShown()) {
+                                    btn.performClick();
+                                }
+                            }
+                        } catch (Exception ignored) {}
+                    }, 50);
+                }
+            };
+            view.getViewTreeObserver().addOnWindowFocusChangeListener(focusChangeListener);
+        }
 
-        // 🌟 FIX 1: Fragment level par "About Nova" label ko surgically "About Apex" mein rename kiya gaya hai!
-        // Isse XML file ko chhede bina UI par "About Apex" ekdum perfect aane lagega.
+        if (binding.aboutText != null) {
+            binding.aboutText.setText(InfoCenter.replaceName(requireActivity(), R.string.about_tab));
+        }
+        if (binding.aboutButton != null) {
+            binding.aboutButton.setVisibility(View.GONE); 
+        }
+
+        // 🌟 FIX: Branding "About Nova" se badal kar "About Apex" runtime reflection layout layout se configure kiya
         if (binding != null) {
-            // Agar layout binding mein direct variable name default 'editSettingsButton' ya kisi text block mein hai,
-            // toh resource framework reflection check lagaya hai taaki build smoothly bina kisi variable error ke pass ho sake.
             View rootLayout = binding.getRoot();
             int aboutNovaId = rootLayout.getResources().getIdentifier("about_nova", "id", rootLayout.getContext().getPackageName());
             if (aboutNovaId != 0) {
-                View aboutButton = rootLayout.findViewById(aboutNovaId);
-                if (aboutButton instanceof TextView) {
-                    ((TextView) aboutButton).setText("About Apex");
+                View aboutView = rootLayout.findViewById(aboutNovaId);
+                if (aboutView instanceof TextView) {
+                    ((TextView) aboutView).setText("About Apex");
                 }
             }
         }
 
-        binding.playButton.setOnClickListener(this);
-        if (binding.editSettingsButton != null) {
-            binding.editSettingsButton.setOnClickListener(this);
+        View activityRoot = requireActivity().findViewById(android.R.id.content);
+        if (activityRoot != null) {
+            ImageView topGamingBtn = activityRoot.findViewById(R.id.btn_override_gaming);
+            ImageView topFolderBtn = activityRoot.findViewById(R.id.btn_override_folder);
+            ImageView topJavaBtn = activityRoot.findViewById(R.id.btn_override_java);
+            ImageView topShareBtn = activityRoot.findViewById(R.id.btn_override_share);
+            TextView topTitleView = activityRoot.findViewById(R.id.txt_override_title);
+
+            if (topTitleView != null) {
+                topTitleView.setText("Apex Launcher"); // Branding update
+                topTitleView.setVisibility(View.VISIBLE);
+            }
+
+            if (topGamingBtn != null) {
+                topGamingBtn.setOnClickListener(v -> {
+                    ViewAnimUtils.setViewAnim(topGamingBtn, Animations.Pulse);
+                    ZHTools.swapFragmentWithAnim(this, ControlButtonFragment.class, ControlButtonFragment.TAG, null);
+                });
+            }
+
+            if (topFolderBtn != null) {
+                topFolderBtn.setOnClickListener(v -> {
+                    ViewAnimUtils.setViewAnim(topFolderBtn, Animations.Pulse);
+                    Bundle bundle = new Bundle();
+                    bundle.putString(FilesFragment.BUNDLE_LIST_PATH, PathManager.DIR_GAME_HOME);
+                    ZHTools.swapFragmentWithAnim(this, FilesFragment.class, FilesFragment.TAG, bundle);
+                });
+            }
+
+            if (topJavaBtn != null) {
+                topJavaBtn.setOnClickListener(v -> {
+                    ViewAnimUtils.setViewAnim(topJavaBtn, Animations.Pulse);
+                    runInstallerWithConfirmation(false);
+                });
+                topJavaBtn.setOnLongClickListener(v -> {
+                    runInstallerWithConfirmation(true);
+                    return true;
+                });
+            }
+
+            if (topShareBtn != null) {
+                topShareBtn.setOnClickListener(v -> {
+                    ViewAnimUtils.setViewAnim(topShareBtn, Animations.Pulse);
+                    ZHTools.shareLogs(requireActivity());
+                });
+            }
         }
-        binding.versionLayout.setOnClickListener(this);
-        binding.accountsLayout.setOnClickListener(this);
 
-        updateAccountUI();
-        updateVersionUI(VersionsManager.INSTANCE.getCurrentVersion());
+        if (binding.customControlButton != null) {
+            binding.customControlButton.setOnClickListener(v -> ZHTools.swapFragmentWithAnim(this, ControlButtonFragment.class, ControlButtonFragment.TAG, null));
+        }
+        if (binding.openMainDirButton != null) {
+            binding.openMainDirButton.setOnClickListener(v -> {
+                Bundle bundle = new Bundle();
+                bundle.putString(FilesFragment.BUNDLE_LIST_PATH, PathManager.DIR_GAME_HOME);
+                ZHTools.swapFragmentWithAnim(this, FilesFragment.class, FilesFragment.TAG, bundle);
+            });
+        }
+        if (binding.installJarButton != null) {
+            binding.installJarButton.setOnClickListener(v -> runInstallerWithConfirmation(false));
+            binding.installJarButton.setOnLongClickListener(v -> {
+                runInstallerWithConfirmation(true);
+                return true;
+            });
+        }
+        if (binding.shareLogsButton != null) {
+            binding.shareLogsButton.setOnClickListener(v -> ZHTools.shareLogs(requireActivity()));
+        }
 
-        binding.getRoot().getViewTreeObserver().addOnGlobalLayoutListener(this);
-        EventBus.getDefault().register(this);
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding.getRoot().getViewTreeObserver().removeOnGlobalLayoutListener(this);
-        EventBus.getDefault().unregister(this);
-        binding = null;
-    }
-
-    private void updateAccountUI() {
-        if (binding == null) return;
-        InfoCenter.INSTANCE.getAccountManager().getCurrentAccountAsync().observe(getViewLifecycleOwner(), account -> {
-            if (account != null) {
-                binding.accountName.setText(account.getProfileName());
+        binding.version.setOnClickListener(v -> {
+            if (!isTaskRunning()) {
+                ZHTools.swapFragmentWithAnim(this, VersionsListFragment.class, VersionsListFragment.TAG, null);
             } else {
-                binding.accountName.setText(R.string.no_account);
+                ViewAnimUtils.setViewAnim(binding.version, Animations.Shake);
+                TaskExecutors.runInUIThread(() -> Toast.makeText(requireContext(), R.string.version_manager_task_in_progress, Toast.LENGTH_SHORT).show());
             }
         });
+        
+        if (binding.editSettingsButton != null) {
+            binding.editSettingsButton.setOnClickListener(v -> {
+                if (!isTaskRunning()) {
+                    ViewAnimUtils.setViewAnim(binding.editSettingsButton, Animations.Pulse);
+                    ZHTools.swapFragmentWithAnim(this, VersionManagerFragment.class, VersionManagerFragment.TAG, null);
+                } else {
+                    ViewAnimUtils.setViewAnim(binding.editSettingsButton, Animations.Shake);
+                    TaskExecutors.runInUIThread(() -> Toast.makeText(requireContext(), R.string.version_manager_task_in_progress, Toast.LENGTH_SHORT).show());
+                }
+            });
+        }
+
+        if (binding.novaDiscord != null) {
+            binding.novaDiscord.setOnClickListener(v -> {
+                android.content.Intent intent = new android.content.Intent(android.content.Intent.ACTION_VIEW, android.net.Uri.parse("https://discord.gg/xFpfUufXg3"));
+                startActivity(intent);
+            });
+        }
+        if (binding.novaWebsite != null) {
+            binding.novaWebsite.setOnClickListener(v -> {
+                android.content.Intent intent = new android.content.Intent(android.content.Intent.ACTION_VIEW, android.net.Uri.parse("https://YOUR_WEBSITE.com"));
+                startActivity(intent);
+            });
+        }
+        if (binding.novaGithub != null) {
+            binding.novaGithub.setOnClickListener(v -> {
+                android.content.Intent intent = new android.content.Intent(android.content.Intent.ACTION_VIEW, android.net.Uri.parse("https://github.com/SadlyNova"));
+                startActivity(intent);
+            });
+        }
+
+        binding.managerProfileButton.setOnClickListener(v -> {
+            if (!isTaskRunning()) {
+                ViewAnimUtils.setViewAnim(binding.managerProfileButton, Animations.Pulse);
+                ZHTools.swapFragmentWithAnim(this, VersionManagerFragment.class, VersionManagerFragment.TAG, null);
+            } else {
+                ViewAnimUtils.setViewAnim(binding.managerProfileButton, Animations.Shake);
+                TaskExecutors.runInUIThread(() -> Toast.makeText(requireContext(), R.string.version_manager_task_in_progress, Toast.LENGTH_SHORT).show());
+            }
+        });
+
+        binding.playButton.setOnClickListener(v -> EventBus.getDefault().post(new LaunchGameEvent()));
+
+        if (binding.playButtonsLayout != null) {
+            binding.playButtonsLayout.setVisibility(View.VISIBLE);
+        }
+        if (binding.playButton != null) {
+            binding.playButton.setVisibility(View.VISIBLE);
+        }
+
+        binding.versionName.setSelected(true);
+        binding.versionInfo.setSelected(true);
+
+        refreshCurrentVersion();
     }
 
-    private void updateVersionUI(@Nullable Version version) {
-        if (binding == null) return;
+    private void refreshCurrentVersion() {
+        Version version = VersionsManager.INSTANCE.getCurrentVersion();
+        int versionInfoVisibility;
         if (version != null) {
             binding.versionName.setText(version.getVersionName());
             VersionInfo versionInfo = version.getVersionInfo();
             if (versionInfo != null) {
-                VersionIconUtils.setVersionIcon(binding.versionIcon, versionInfo.getType());
-            } else {
-                binding.versionIcon.setImageResource(R.drawable.ic_game_unknown);
-            }
+                binding.versionInfo.setText(versionInfo.getInfoString());
+                versionInfoVisibility = View.VISIBLE;
+            } else versionInfoVisibility = View.GONE;
+
+            new VersionIconUtils(version).start(binding.versionIcon);
+            binding.managerProfileButton.setVisibility(View.VISIBLE);
         } else {
-            binding.versionName.setText(R.string.no_version);
-            binding.versionIcon.setImageResource(R.drawable.ic_game_unknown);
+            binding.versionName.setText(R.string.version_no_versions);
+            binding.managerProfileButton.setVisibility(View.GONE);
+            versionInfoVisibility = View.GONE;
+        }
+        binding.versionInfo.setVisibility(versionInfoVisibility);
+    }
+
+    @Subscribe()
+    public void event(RefreshVersionsEvent event) {
+        if (event.getMode() == END) {
+            TaskExecutors.runInUIThread(this::refreshCurrentVersion);
         }
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onAccountUpdate(AccountUpdateEvent event) {
-        updateAccountUI();
-    }
-
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onRefreshVersions(RefreshVersionsEvent event) {
-        if (event.mode == END) {
-            updateVersionUI(VersionsManager.INSTANCE.getCurrentVersion());
-        }
+    public void event(AccountUpdateEvent event) {
+        if (accountViewWrapper != null) accountViewWrapper.refreshAccountInfo();
     }
 
     @Override
-    public void onClick(View v) {
-        if (v == binding.playButton) {
-            Version currentVersion = VersionsManager.INSTANCE.getCurrentVersion();
-            if (currentVersion == null) {
-                Toast.makeText(requireContext(), R.string.no_version, Toast.LENGTH_SHORT).show();
-                return;
-            }
-            EventBus.getDefault().post(new LaunchGameEvent(currentVersion));
-        } else if (v == binding.editSettingsButton) {
-            triggerFragmentShift(new ControlButtonFragment());
-        } else if (v == binding.versionLayout) {
-            ZHTools.openVersionSelector(requireActivity());
-        } else if (v == binding.accountsLayout) {
-            ZHTools.openAccountSelector(requireActivity());
-        } else {
-            // Check dynamic about button clicks safely
-            View rootLayout = binding.getRoot();
-            int aboutNovaId = rootLayout.getResources().getIdentifier("about_nova", "id", rootLayout.getContext().getPackageName());
-            if (aboutNovaId != 0 && v.getId() == aboutNovaId) {
-                triggerFragmentShift(new AboutFragment());
-            }
+    public void onDestroyView() {
+        if (getView() != null && focusChangeListener != null) {
+            getView().getViewTreeObserver().removeOnWindowFocusChangeListener(focusChangeListener);
         }
+        super.onDestroyView();
     }
 
     @Override
-    public void onGlobalLayout() {
-        if (binding == null) return;
-        binding.getRoot().getViewTreeObserver().removeOnGlobalLayoutListener(this);
-        adjustPlayLayoutMargin();
-    }
+    public void onStart() { super.onStart(); EventBus.getDefault().register(this); }
+    @Override
+    public void onStop() { super.onStop(); EventBus.getDefault().unregister(this); }
 
-    private void adjustPlayLayoutMargin() {
-        if (binding == null) return;
-        int height = binding.getRoot().getHeight();
-        if (height == 0) return;
-        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) binding.playLayout.getLayoutParams();
-        params.topMargin = height / 3;
-        binding.playLayout.setLayoutParams(params);
+    private void runInstallerWithConfirmation(boolean isCustomArgs) {
+        if (ProgressKeeper.getTaskCount() == 0)
+            Tools.installMod(requireActivity(), isCustomArgs);
+        else
+            Toast.makeText(requireContext(), R.string.tasks_ongoing, Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void slideIn(AnimPlayer animPlayer) {
-        if (binding == null) return;
         View activityRoot = requireActivity().findViewById(android.R.id.content);
         if (activityRoot != null) {
             View topContainer = activityRoot.findViewById(R.id.btn_override_gaming);
@@ -183,11 +305,10 @@ public class MainMenuFragment extends SubassemblyFragment implements View.OnClic
                 animPlayer.apply(new AnimPlayer.Entry(topTitle, Animations.BounceInDown));
             }
         }
-
         if (binding.centeredLogosContainer != null) {
             animPlayer.apply(new AnimPlayer.Entry(binding.centeredLogosContainer, Animations.BounceInDown));
         }
-        animPlayer.apply(new AnimPlayer.Entry(binding.playLayout, Animations.BounceInRight));
+        animPlayer.apply(new AnimPlayer.Entry(binding.playLayout, Animations.BounceInLeft));
         if (binding.playButtonsLayout != null) {
             animPlayer.apply(new AnimPlayer.Entry(binding.playButtonsLayout, Animations.BounceEnlarge));
         }
@@ -198,7 +319,6 @@ public class MainMenuFragment extends SubassemblyFragment implements View.OnClic
 
     @Override
     public void slideOut(AnimPlayer animPlayer) {
-        if (binding == null) return;
         View activityRoot = requireActivity().findViewById(android.R.id.content);
         if (activityRoot != null) {
             View topContainer = activityRoot.findViewById(R.id.btn_override_gaming);
@@ -210,7 +330,6 @@ public class MainMenuFragment extends SubassemblyFragment implements View.OnClic
                 animPlayer.apply(new AnimPlayer.Entry(topTitle, Animations.FadeOutUp));
             }
         }
-
         if (binding.centeredLogosContainer != null) {
             animPlayer.apply(new AnimPlayer.Entry(binding.centeredLogosContainer, Animations.FadeOutUp));
         }
